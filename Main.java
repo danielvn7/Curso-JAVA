13c626e7c7edbad814e5e25465d988ee0658add1
Daniel Venancio <115376164+danielvn7@users.noreply.github.com>
2022-10-21 12:06:04 -0300

    curso de programação

---
+++ Main.java
@@ -0,0 +1,56 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Exemplo 1");
+		int x, y;
+		x = 5;
+		y = 2 * x;
+		System.out.println(x);
+		System.out.println(y);
+		
+		System.out.printf("%n");
+		
+		System.out.println("Exemplo 2");
+		int x1;
+		double y1;
+		x1 = 5;
+		y1 = 2 * x1;
+		System.out.println(x1);
+		System.out.println(y1);
+		
+		System.out.printf("%n");
+		
+		System.out.println("Exemplo 3");
+		double b, B, h, area;
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+		area = (b + B) / 2.0 * h;
+		System.out.println(area);
+		
+		System.out.printf("%n");
+		
+		System.out.println("Exemplo 4 (2f)");
+		float b1, B1, h1, area1;
+		b1 = 6f;
+		B1 = 8f;
+		h1 = 5f;
+		area1 = (b1 + B1) / 2f * h1;
+		System.out.println(area1);
+		
+		System.out.printf("%n");
+		
+		System.out.println("Exemplo 5");
+		int a, b11;
+		double resultado;
+		a = 5;
+		b11 = 2;
+		resultado = a / b11;
+		System.out.println(resultado);
+		
+	}
+
+}
