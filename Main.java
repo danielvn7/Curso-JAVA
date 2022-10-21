f1cf22470f7925bd0aa2e7ae6b312cf4f66c0ad5
Daniel Venancio <115376164+danielvn7@users.noreply.github.com>
2022-10-21 12:08:20 -0300

    Primeiros passos

--- Main.java
+++ Main.java
@@ -1,55 +1,77 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("Exemplo 1");
-		int x, y;
-		x = 5;
-		y = 2 * x;
+		System.out.println("Olá Mundo!");
+		System.out.println("Bom dia!");
+		
+		System.out.printf("%n");
+		
+		int y = 32;
+		double x = 10.35784;
+		
+		System.out.println("EXEMPLO EM int ");
+		System.out.println("Gostaria de informar que conheci uma Juliana, de " + y + " Anos ");
+		System.out.println(y + " Anos?");
+		
+		System.out.printf("%n");
+		
+		System.out.println("EXEMPLO EM double: ");
 		System.out.println(x);
-		System.out.println(y);
+		
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f%n", x);
 		
 		System.out.printf("%n");
 		
-		System.out.println("Exemplo 2");
-		int x1;
-		double y1;
-		x1 = 5;
-		y1 = 2 * x1;
-		System.out.println(x1);
-		System.out.println(y1);
+		Locale.setDefault(Locale.US);// Altera virgula para, ponto.
+		System.out.printf("%.4f%n", x);
 		
 		System.out.printf("%n");
 		
-		System.out.println("Exemplo 3");
-		double b, B, h, area;
-		b = 6.0;
-		B = 8.0;
-		h = 5.0;
-		area = (b + B) / 2.0 * h;
-		System.out.println(area);
+		System.out.println("EXEMPLO COM 2 e 4 DECIOMAIS");
+		System.out.println("RESULTADO = " + x + " METROS");
+		System.out.printf("RESULTADO = %.2f METROS%n", x);
+		System.out.printf("RESULTADO = %.4f METROS%n", x);
 		
 		System.out.printf("%n");
 		
-		System.out.println("Exemplo 4 (2f)");
-		float b1, B1, h1, area1;
-		b1 = 6f;
-		B1 = 8f;
-		h1 = 5f;
-		area1 = (b1 + B1) / 2f * h1;
-		System.out.println(area1);
+		System.out.println("Regras geral para PRINTF");
+		String nome = "Juliana";
+		int idade = 32;
+		double renda = 4000.0;
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+		// %.2f = ponto flutuante (DOUBLE)
+		// %d = inteiro (INT)
+		// %s = texto (STRING)
+		// %n = quebras de linha
 		
 		System.out.printf("%n");
 		
-		System.out.println("Exemplo 5");
-		int a, b11;
-		double resultado;
-		a = 5;
-		b11 = 2;
-		resultado = a / b11;
-		System.out.println(resultado);
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		byte age = 30;
+		int code = 5290;
+		char gender = 'F';
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		System.out.println("Products:");
+		System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+		System.out.printf("%s, which price is $ %.2f%n", product2, price2);
+		System.out.println();
+		System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code, gender);
+		System.out.println();
+		System.out.printf("Measue with eight decimal places: %.8f%n", measure);
+		System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+		Locale.setDefault(Locale.US);
+		System.out.printf("US decimal point: %.3f%n", measure);
+		
+		
+		
+		
 		
 	}
 
